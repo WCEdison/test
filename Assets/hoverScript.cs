7510bf26dbca97cc17021e132cbcb6a42ef86a12
Franco Test <test@abc.com>
2018-04-06 17:47:19 +0800

    hover and snap

---
+++ Assets/hoverScript.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoverScript : MonoBehaviour
+{
+
+    public Material transparent, solid;
+   
+    Vector3 dist;
+    float posX, posY;
+    public bool drag = true;
+    bool snap = false;
+
+    public bool Snap
+    {
+        get
+        {
+            return snap;
+        }
+
+        set
+        {
+            snap = value;
+        }
+    }
+
+    private void Start()
+    {
+        // Find current position
+        dist = Camera.main.WorldToScreenPoint(transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        posY = Input.mousePosition.y - dist.y;
+
+    }
+    void Update()
+    {
+        if (drag)
+        {
+            // Given current position and user is dragging, update position according to mouse movement
+            Vector3 currentPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, dist.z);
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(currentPos);
+
+            transform.position = worldPos;
+        }
+    }
+    public void StartDragging()
+    {
+        // Start dragging if the mouse did not snap to a snap point
+        if (!Snap && !drag) drag = true;
+    }
+
+    public void EndDragging(Vector3 NewPosition)
+    {
+        // Stop dragging if the mouse reached into a snap point
+        if (drag) drag = false;
+        transform.position = new Vector3(NewPosition.x, NewPosition.y - 0.3f, NewPosition.z);
+    }
+
+    public void SnapToObject()
+    {
+        Snap = true;
+        // Change shader and snap
+        GetComponent<Renderer>().material = solid;
+    }
+}
---
+++ Assets/snap.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snap : MonoBehaviour {
+
+    
+    public GameObject hover;
+    public GameObject[] SnapPoints;
+    hoverScript h;
+    public bool didSpawn = false;
+    private bool didSpawnHover = false;
+
+    // Update is called once per frame
+    void Update () {
+        RaycastHit hit;
+
+        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Does the ray intersect any objects excluding the player layer
+        
+        if (Physics.Raycast(r, out hit, 100.0f))
+        {
+
+            foreach (GameObject sp in SnapPoints)
+            {
+
+                if (hit.collider.name.Equals(sp.name) && didSpawn)
+                {
+                    // Mouse is hovering over a snap point && a hovering object is spawned
+                    // Snap hovering object to snap point
+                    h.EndDragging(sp.transform.position);
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        h.SnapToObject();
+                        didSpawn = false;
+                    }
+                }
+
+            }
+
+        }
+        else
+        {
+            // Did not hit any object
+            if (!h.Snap)
+            {
+                h.StartDragging();
+            }
+        }
+    }
+    
+    public void SpawnHover()
+    {
+        if (!didSpawnHover)
+        {
+            GameObject hs = GameObject.Instantiate(hover, new Vector3(transform.localPosition.x, transform.localPosition.y + 1.2f, transform.localPosition.z + 4.0f), new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
+            didSpawn = true;
+            h = hs.GetComponent<hoverScript>();
+            didSpawnHover = true;
+        }
+    }
+    
+}
